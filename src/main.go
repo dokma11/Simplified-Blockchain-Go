8d2f097c881bcebbee3b5abc6d89d84bd72cf338
Vukašin Dokmanović <vule.dok@gmail.com>
2025-02-12 09:14:16 +0100

    improve cli

--- src/main.go
+++ src/main.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"github.com/boltdb/bolt"
 )
 
 func main() {
 	bc := NewBlockchain()
+	defer func(DB *bolt.DB) {
+		err := DB.Close()
+		if err != nil {
+			fmt.Printf("Error occurred while closing the DB: %s", err)
+		}
+	}(bc.DB)
 
-	bc.AddBlock("Send 1 BTC to A")
-	bc.AddBlock("Send 2 more BTC to A")
-
-	for _, block := range bc.Blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PreviousHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
